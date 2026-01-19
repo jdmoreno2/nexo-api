1ba1634f8b39857e7660bca6b5e3cd16177c2485
Dario Moreno <dario_moreno10@hotmail.com>
2026-01-19 11:45:53 -0500

    F25-Se correge nombre del modulo suscriptores y cada una de las clases, propiedades, constantes e importaciones

--- src/modules/suscribers/dto/responses/response-suscriber.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from "@nestjs/swagger";
-import { UpdateSuscriberDto } from "../request/update-suscriber.dto";
-
-export class ResponseSuscriberDto extends PartialType(UpdateSuscriberDto) {
-
-}
