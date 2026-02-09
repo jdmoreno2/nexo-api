ac47744de847a8b3405cf6df496726872e0c598c
Dario Moreno <dario_moreno10@hotmail.com>
2026-02-09 10:24:06 -0500

    T46 - Feat - Se crea CRUD para el modulo tipos de ordenes

--- src/modules/orders_types/dto/request/create-orders_type.dto.ts
+++ src/modules/orders_types/dto/request/create-orders_type.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger"
-import { IsNotEmpty, IsOptional, IsString, Validate } from "class-validator"
+import { IsInt, IsNotEmpty, IsOptional, IsString, Validate } from "class-validator"
+import { OrdersTypesAlreadyExistsConstraint } from "../../decorators/orders-types.validator"
+import { SubscriberExistsConstraint } from "src/modules/subscribers/decorators/subscriber.validator"
 
 
 export class CreateOrdersTypeDto {
@@ -10,14 +12,23 @@ export class CreateOrdersTypeDto {
   @IsString
     ({ message: 'Debe ser ingresado un texto' })
   @IsNotEmpty({ message: 'El nombre del tipo de orden es obligatorio' })
-  @Validate(IdentificationTypeAlreadyExistsConstraint)
+  @Validate(OrdersTypesAlreadyExistsConstraint)
   name: string
 
   @ApiPropertyOptional({
     description: 'Descripción del tipo de identificacion',
-    example: 'Numero de Cedula', required: false
+    example: 'Para ordenes de Servicio', required: false
   })
   @IsOptional()
   @IsString({ message: 'Debe ser ingresado un texto' })
   description?: string
+
+  @ApiProperty({
+    description: 'ID del suscriptor al que pertenece el rol',
+    example: 1
+  })
+  @IsNotEmpty({ message: 'Faltan datos necesario: subscribers_id.' })
+  @IsInt({ message: 'Formato de Datos invalido: subscribers_id debe ser un numero entero.' })
+  @Validate(SubscriberExistsConstraint)
+  subscribers_id: number;
 }
--- src/modules/orders_types/orders_types.service.ts
+++ src/modules/orders_types/orders_types.service.ts
@@ -1,30 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateOrdersTypeDto } from './dto/request/create-orders_type.dto';
 import { UpdateOrdersTypeDto } from './dto/request/update-orders_type.dto';
+import { GenericResponsesDto } from 'src/common/dto/generic-response.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { OrdersType } from './entities/orders_type.entity';
+import { ILike, Repository } from 'typeorm';
+import { PaginationDto, PaginationRequestMetaDto } from 'src/common/dto/pagination-response.dto';
+import { ResponseOrdersTypesDto } from './dto/response/response-orders-types.dto';
 
 @Injectable()
 export class OrdersTypesService {
-  create(createOrdersTypeDto: CreateOrdersTypeDto) {
-    return 'This action adds a new ordersType';
+  constructor(
+    @InjectRepository(OrdersType)
+    private readonly ordersTypesRepository: Repository<OrdersType>
+  ) { }
+
+  async create(createOrdersTypeDto: CreateOrdersTypeDto): Promise<GenericResponsesDto> {
+    if (!await this.ordersTypesRepository.save(createOrdersTypeDto)) {
+      throw new BadRequestException('Error al crear el tipo de orden')
+    }
+    return { message: 'Tipo de orden Creada Exitosamente', statusCode: 201, error: '' };
   }
 
-  findAll() {
-    return `This action returns all ordersTypes`;
+  async findAll(meta: PaginationRequestMetaDto): Promise<PaginationDto<ResponseOrdersTypesDto>> {
+    const page = meta?.page || 1;
+    const limit = meta?.limit || 10;
+    const skit = (page - 1) * limit;
+
+    const [result, total] = await this.ordersTypesRepository.findAndCount({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        status: true,
+        subscribers_id: true
+      },
+      where: meta.search ? [
+        { name: ILike(`%${meta.search}%`) },
+        { description: ILike(`%${meta.search}%`) },
+      ] : {},
+      order: {
+        [meta.orderBy || 'id']: meta.order || 'ASC'
+      },
+      skip: skit,
+      take: limit
+    });
+
+    return {
+      data: result,
+      meta: {
+        page: page,
+        limit: limit,
+        total: total,
+        totalPages: Math.ceil(total / limit)
+      }
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ordersType`;
+  async findOne(id: number) {
+    return this.ordersTypesRepository.findOneBy({ id });
   }
 
-  findOneByName(name: string) {
-    return `This action returns a #${name} ordersType`;
+  async findOneByName(name: string) {
+    return this.ordersTypesRepository.findOneBy({ name: ILike(name.toLowerCase()) });
   }
 
-  update(id: number, updateOrdersTypeDto: UpdateOrdersTypeDto) {
-    return `This action updates a #${id} ordersType`;
+  async update(id: number, updateOrdersTypeDto: UpdateOrdersTypeDto): Promise<GenericResponsesDto> {
+    const updatedBranch = await this.ordersTypesRepository.update(id, updateOrdersTypeDto);
+    if (updatedBranch.affected === 0) throw new BadRequestException('Error al actualizar el Tipo de orden');
+    return { message: 'Tipo de orden Actualizado', statusCode: 200, error: '' };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} ordersType`;
+  async remove(id: number): Promise<GenericResponsesDto> {
+    const removedBranch = await this.ordersTypesRepository.update(id, { status: 0 });
+    if (removedBranch.affected === 0) throw new BadRequestException('Error al eliminar el Tipo de orden');
+    return { message: 'Tipo de orden Eliminado', statusCode: 200, error: '' };
   }
 }
