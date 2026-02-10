1d853db2b63faeab62890fd375bb81c6f61a9991
Dario Moreno <dario_moreno10@hotmail.com>
2026-02-10 11:30:38 -0500

    T56 - Feat - Se crea Modulo Preguntas

---
+++ src/modules/questions/questions.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { QuestionsService } from './questions.service';
+import { QuestionsController } from './questions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Question } from './entities/question.entity';
+import { FormsModule } from '../forms/forms.module';
+import { QuestionsTypesModule } from '../questions_types/questions_types.module';
+import { QuestionsExistsConstraint, QuestionsExistsPipe } from './decorators/questios.validator';
+import { FormExistsConstraint } from '../forms/decorators/forms.validator';
+import { QuestionsTypesExistsConstraint } from '../questions_types/decorators/questions-types.validator';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Question]),
+    FormsModule,
+    QuestionsTypesModule
+  ],
+  controllers: [QuestionsController],
+  providers: [
+    QuestionsService,
+    QuestionsExistsConstraint,
+    QuestionsExistsPipe,
+    FormExistsConstraint,
+    QuestionsTypesExistsConstraint
+  ],
+  exports: [QuestionsService, QuestionsExistsConstraint]
+})
+export class QuestionsModule { }
