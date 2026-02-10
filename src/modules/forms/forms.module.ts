c22c0d455ac4c23fe6509e68858daa92a54cab5f
Dario Moreno <dario_moreno10@hotmail.com>
2026-02-10 10:28:14 -0500

    T54 - Feat - Se crea crud para el modulo formularios

--- src/modules/forms/forms.module.ts
+++ src/modules/forms/forms.module.ts
@@ -3,12 +3,12 @@ import { FormsService } from './forms.service';
 import { FormsController } from './forms.controller';
 import { FormAlreadyExistsConstraint, FormExistsConstraint, FormExistsPipe } from './decorators/forms.validator';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Order } from '../orders/entities/order.entity';
 import { SubscribersModule } from '../subscribers/subscribers.module';
 import { SubscriberExistsConstraint } from '../subscribers/decorators/subscriber.validator';
+import { Form } from './entities/form.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order]), SubscribersModule],
+  imports: [TypeOrmModule.forFeature([Form]), SubscribersModule],
   controllers: [FormsController],
   providers: [
     FormsService,
--- src/modules/forms/forms.service.ts
+++ src/modules/forms/forms.service.ts
@@ -1,30 +1,79 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateFormDto } from './dto/request/create-form.dto';
 import { UpdateFormDto } from './dto/request/update-form.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Form } from './entities/form.entity';
+import { ILike, Repository } from 'typeorm';
+import { GenericResponsesDto } from 'src/common/dto/generic-response.dto';
+import { PaginationDto, PaginationRequestMetaDto } from 'src/common/dto/pagination-response.dto';
+import { ResponseFormsDto } from './dto/response/response-forms.dto';
 
 @Injectable()
 export class FormsService {
-  create(createFormDto: CreateFormDto) {
-    return 'This action adds a new form';
+  constructor(
+    @InjectRepository(Form)
+    private readonly formsRepository: Repository<Form>
+  ) { }
+
+  async create(createFormDto: CreateFormDto): Promise<GenericResponsesDto> {
+    if (!await this.formsRepository.save(createFormDto)) {
+      throw new BadRequestException('Error al crear el formulario')
+    }
+    return { message: 'Formulario Creado Exitosamente', statusCode: 201, error: '' };
   }
 
-  findAll() {
-    return `This action returns all forms`;
+  async findAll(meta: PaginationRequestMetaDto): Promise<PaginationDto<ResponseFormsDto>> {
+    const page = meta?.page || 1;
+    const limit = meta?.limit || 10;
+    const skit = (page - 1) * limit;
+
+    const [result, total] = await this.formsRepository.findAndCount({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        status: true,
+        subscribers_id: true
+      },
+      where: meta.search ? [
+        { name: ILike(`%${meta.search}%`) },
+        { description: ILike(`%${meta.search}%`) },
+      ] : {},
+      order: {
+        [meta.orderBy || 'id']: meta.order || 'ASC'
+      },
+      skip: skit,
+      take: limit
+    });
+
+    return {
+      data: result,
+      meta: {
+        page: page,
+        limit: limit,
+        total: total,
+        totalPages: Math.ceil(total / limit)
+      }
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} form`;
+  async findOne(id: number): Promise<Form | null> {
+    return this.formsRepository.findOneBy({ id });
   }
 
-  findOneByName(name: string) {
-    return `This action returns a #${name} form`;
+  async findOneByName(name: string): Promise<Form | null> {
+    return this.formsRepository.findOneBy({ name: ILike(name.toLowerCase()) });
   }
 
-  update(id: number, updateFormDto: UpdateFormDto) {
-    return `This action updates a #${id} form`;
+  async update(id: number, updateFormDto: UpdateFormDto): Promise<GenericResponsesDto> {
+    const updatedForm = await this.formsRepository.update(id, updateFormDto);
+    if (updatedForm.affected === 0) throw new BadRequestException('Error al actualizar el Formulario');
+    return { message: 'Formulario Actualizado', statusCode: 200, error: '' };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} form`;
+  async remove(id: number): Promise<GenericResponsesDto> {
+    const removedBranch = await this.formsRepository.update(id, { status: 0 });
+    if (removedBranch.affected === 0) throw new BadRequestException('Error al eliminar el Formulario');
+    return { message: 'Formulario Eliminado', statusCode: 200, error: '' };
   }
 }
