16a433224b40bd68ef887982e675e8f0cbf3d07e
Dario Moreno <dario_moreno10@hotmail.com>
2026-03-03 10:42:13 -0500

    fix: Corrige el nombre de la clase ResponseQuestionsDto en el DTO y el controlador

--- src/modules/questions/questions.controller.ts
+++ src/modules/questions/questions.controller.ts
@@ -4,7 +4,7 @@ import { CreateQuestionDto } from './dto/request/create-question.dto';
 import { UpdateQuestionDto } from './dto/request/update-question.dto';
 import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GenericResponsesDto } from 'src/common/dto/generic-response.dto';
-import { ResponseQuestiosDto } from './dto/response/response-questions.dto';
+import { ResponseQuestionsDto } from './dto/response/response-questions.dto';
 import { PaginationDto, PaginationRequestMetaDto } from 'src/common/dto/pagination-response.dto';
 import { QuestionsExistsPipe } from './decorators/questios.validator';
 
@@ -50,7 +50,7 @@ export class QuestionsController {
     summary: 'Endpoint para obtener todos las Preguntas',
     description: 'Permite obtener una lista paginada de todos las Preguntas registrados en el sistema.'
   })
-  @ApiOkResponse({ description: 'Lista paginada de Preguntas', type: PaginationDto<ResponseQuestiosDto> })
+  @ApiOkResponse({ description: 'Lista paginada de Preguntas', type: PaginationDto<ResponseQuestionsDto> })
   findAll(@Query() meta: PaginationRequestMetaDto) {
     return this.questionsService.findAll(meta);
   }
@@ -60,7 +60,7 @@ export class QuestionsController {
     summary: 'Endpoint para obtener una Pregunta por ID',
     description: 'Permite obtener la información de una Pregunta existente utilizando su ID.'
   })
-  @ApiOkResponse({ description: 'Pregunta obtenida exitosamente', type: ResponseQuestiosDto })
+  @ApiOkResponse({ description: 'Pregunta obtenida exitosamente', type: ResponseQuestionsDto })
   findOne(@Param('id', QuestionsExistsPipe) id: string) {
     return this.questionsService.findOne(+id);
   }
