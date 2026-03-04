d8893970258d028c6b5b8de7e34a508395edf067
Dario Moreno <dario_moreno10@hotmail.com>
2026-03-04 15:26:07 -0500

    feat: Se quita propiedad responses de tipo objeto por array de string heradado de la clase CreateQuestionDto

--- src/modules/questions/dto/request/update-question.dto.ts
+++ src/modules/questions/dto/request/update-question.dto.ts
@@ -1,12 +1,8 @@
-import { ApiProperty, ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateQuestionDto } from './create-question.dto';
-import { IsArray, IsNotEmpty, IsNumber, IsOptional, Max, Min, ValidateNested } from 'class-validator';
-import { UpdateResponseDto } from 'src/modules/responses/dto/request/update-response.dto';
-import { Type } from 'class-transformer';
+import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 
-export class UpdateQuestionsResponseDto extends OmitType(UpdateResponseDto, ['questions_id'] as const) { }
-
-export class UpdateQuestionDto extends PartialType(OmitType(CreateQuestionDto, ['responses'] as const)) {
+export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
   @ApiPropertyOptional({ description: 'ID', example: 1 })
   @IsOptional()
   @IsNumber({}, { message: 'Debe ser un número entero' })
@@ -19,14 +15,4 @@ export class UpdateQuestionDto extends PartialType(OmitType(CreateQuestionDto, [
   @Max(1, { message: 'El dato enviado excede el rango permitido: máximo 1' })
   status?: number
 
-  @ApiPropertyOptional({
-    description: 'Respuestas',
-    type: [UpdateQuestionsResponseDto]
-  })
-  @IsOptional()
-  @IsNotEmpty({ message: 'Faltan datos necesario: responses.' })
-  @IsArray({ message: 'responses debe ser un arreglo.' })
-  @ValidateNested({ each: true })
-  @Type(() => UpdateQuestionsResponseDto)
-  responses?: UpdateQuestionsResponseDto[]
 }
