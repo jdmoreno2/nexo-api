6c00169c3497b3265480d3c2c28cf219212bcfd3
Dario Moreno <dario_moreno10@hotmail.com>
2026-03-04 16:07:57 -0500

    Feat - Se añade importacion del modulo OrdersType en el Modulo Form

--- src/modules/forms/forms.module.ts
+++ src/modules/forms/forms.module.ts
@@ -7,12 +7,15 @@ import { SubscribersModule } from '../subscribers/subscribers.module';
 import { SubscriberExistsConstraint } from '../subscribers/decorators/subscriber.validator';
 import { Form } from './entities/form.entity';
 import { QuestionsModule } from '../questions/questions.module';
+import { OrdersTypesModule } from '../orders_types/orders_types.module';
+import { OrdersTypesExistsConstraint } from '../orders_types/decorators/orders-types.validator';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Form]),
     SubscribersModule,
-    forwardRef(() => QuestionsModule)
+    forwardRef(() => QuestionsModule),
+    OrdersTypesModule
   ],
   controllers: [FormsController],
   providers: [
@@ -21,6 +24,7 @@ import { QuestionsModule } from '../questions/questions.module';
     FormExistsConstraint,
     FormExistsPipe,
     SubscriberExistsConstraint,
+    OrdersTypesExistsConstraint,
   ],
   exports: [FormsService, FormExistsConstraint],
 })
