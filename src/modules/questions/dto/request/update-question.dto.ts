42e8c17e63481d71e5a89abd27b66abbb4465eb5
Dario Moreno <dario_moreno10@hotmail.com>
2026-03-04 11:13:08 -0500

    feat: Se actualiza UpdateQuestionDto para incluir respuestas y se modifica la herencia de clases

--- src/modules/questions/dto/request/update-question.dto.ts
+++ src/modules/questions/dto/request/update-question.dto.ts
@@ -1,11 +1,16 @@
-import { ApiProperty, ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional, OmitType, PartialType } from '@nestjs/swagger';
 import { CreateQuestionDto } from './create-question.dto';
-import { IsNumber, IsOptional, Max, Min } from 'class-validator';
+import { IsArray, IsNotEmpty, IsNumber, IsOptional, Max, Min, ValidateNested } from 'class-validator';
+import { UpdateResponseDto } from 'src/modules/responses/dto/request/update-response.dto';
+import { Type } from 'class-transformer';
 
-export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
-  @ApiProperty({ description: 'ID', example: 1 })
+export class UpdateQuestionsResponseDto extends OmitType(UpdateResponseDto, ['questions_id'] as const) { }
+
+export class UpdateQuestionDto extends PartialType(OmitType(CreateQuestionDto, ['responses'] as const)) {
+  @ApiPropertyOptional({ description: 'ID', example: 1 })
+  @IsOptional()
   @IsNumber({}, { message: 'Debe ser un número entero' })
-  id: number
+  id?: number
 
   @ApiPropertyOptional({ description: 'Estado', example: 1, required: false })
   @IsOptional()
@@ -13,4 +18,15 @@ export class UpdateQuestionDto extends PartialType(CreateQuestionDto) {
   @Min(0, { message: 'El dato enviado excede el rango permitido: mínimo 0' })
   @Max(1, { message: 'El dato enviado excede el rango permitido: máximo 1' })
   status?: number
+
+  @ApiPropertyOptional({
+    description: 'Respuestas',
+    type: [UpdateQuestionsResponseDto]
+  })
+  @IsOptional()
+  @IsNotEmpty({ message: 'Faltan datos necesario: responses.' })
+  @IsArray({ message: 'responses debe ser un arreglo.' })
+  @ValidateNested({ each: true })
+  @Type(() => UpdateQuestionsResponseDto)
+  responses?: UpdateQuestionsResponseDto[]
 }
